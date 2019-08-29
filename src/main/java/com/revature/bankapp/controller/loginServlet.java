b18e8126d9ada58b2bfd748125e261072c898b84
deepaselvin <pradeepaselvin97@gmail.com>
2019-08-29 03:52:52 -0400

    Intial commit

---
+++ src/main/java/com/revature/bankapp/controller/loginServlet.java
@@ -0,0 +1,47 @@
+package com.revature.bankapp.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.revature.bankapp.dao.UserDAO;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        PrintWriter out = response.getWriter();
+        out.println("Email:" + email);
+        out.println("Password:" + password );
+        
+        boolean isValidUser=false;
+        try {
+        	UserDAO userDAO = new UserDAO();
+            isValidUser = userDAO.login(email, password);
+        }
+        catch(Exception e) {
+        	e.printStackTrace();
+        }
+        
+        if(isValidUser) {
+        	response.sendRedirect("home.jsp");
+        }
+        else {
+        	response.sendRedirect("index.jsp ? message=InValid Login Ceredentials");
+        }
+	}
+
+	
+	}
+
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2>login page</h2>
+<form action="loginServlet">
+Email: <input type="text" name="email" required />
+<br/>
+Password: <input type="text" name="password" required />
+<br/>
+<button type="submit" >Submit</button>
+</form>
+</body>
+</html>
